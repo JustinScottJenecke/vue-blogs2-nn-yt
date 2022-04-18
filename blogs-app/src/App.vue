2832f8e6c47b3ed9799fcc36f6528f2d3a2abaea
Justin Scott Jenecke <93656319+codespace-justin@users.noreply.github.com>
2022-04-18 10:27:33 +0200

    performs input binding and sets up addBlog form

---
+++ blogs-app/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="app">
+
+    <add-blog/>  
+
+  </div>
+</template>
+
+
+<script>
+  import AddBlog from "./components/AddBlog.vue";
+
+  export default {
+    name: 'App',
+
+    // props
+    props : {
+
+    },
+
+    // components
+    components : {
+      'add-blog' : AddBlog
+    },
+
+    // data
+    data() {
+      return {
+
+      }
+    },
+
+    // methods
+    methods : {
+
+    }
+
+  }
+
+</script>
+
+
+<style>
+  
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  body {
+    margin : 0;
+    padding: 1rem;
+  }
+
+  /* ---- Component Styles ---- */
+    .form {
+        display: flex;
+        flex-direction: column;
+        margin: 0 30vw;
+    }
+
+    .form * {
+      margin: 0.5rem 0;
+    }
+
+    .form input {
+        padding: 0.5rem;
+    }
+
+    .add-title {
+        text-align: center;
+    }
+
+</style>
---
+++ blogs-app/src/components/AddBlog.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="add-blog">
+
+    <h2 class="add-title">
+        Add a new blog post..
+    </h2>
+    <form action="" class="form">
+        <label for="blog-title">Blog Title:</label>
+        <input 
+            v-model="newBlog.title"
+            type="text" 
+            name="blog-title" 
+            required
+        >
+        <label for="blog-content">Blog Content:</label>        
+        <textarea 
+            v-model.lazy="newBlog.content"
+            name="blog-content"
+            cols="30" 
+            rows="3"
+        >
+        </textarea>
+    </form>
+    <div>
+        
+    </div>
+
+    <div id="preview">
+        <h3>
+            Preview Blog
+        </h3>
+        <p>
+            Blog Title: {{newBlog.title}}
+        </p>
+        <p>
+            Blog Content: {{newBlog.content}}
+        </p>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'AddBlog',
+
+    // props
+    props : {
+
+    },
+
+    // components
+    components : {
+
+    },
+
+    // data
+    data() {
+      return {
+        
+        newBlog: {
+            title : "",
+            content : ""
+        }
+
+      }
+    },
+
+    // methods
+    methods : {
+
+    }
+
+  }
+
+</script>
+
+
+<style>
+  
+
+</style>
